07a5e3c66b29a2ce243f38626ca7f877276f4803
FrancoisDotNet <56775634+FrancoisDotNet@users.noreply.github.com>
2020-01-09 12:33:11 +0100

    09/01/2020 12:33:00

--- BlazorWasm/Program.cs
+++ BlazorWasm/Program.cs
@@ -6,10 +6,10 @@ namespace BlazorWasm
     class Program
     {
         static void Main() => BlazorWebAssemblyHost.CreateDefaultBuilder().UseBlazorStartup<Startup>().Build().Run();
-    }
-
-    class Startup
-    {
-        public void Configure(IComponentsApplicationBuilder b) => b.AddComponent<Body>("body");
+    
+        class Startup
+        {
+            public void Configure(IComponentsApplicationBuilder b) => b.AddComponent<Body>("body");
+        }
     }
 }
